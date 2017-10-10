ea9af94a25a251846d96482fed67d60120ddc9f0
Adriano <Adriano@>
2017-10-10 17:22:56 -0300

    Formatação de saídas e desenho de tabela para melhor visualização dos dados.

--- test.py
+++ test.py
@@ -7,44 +7,48 @@ handler = DBHandler()
 handler.connect()
 handler.create_table()
 
-def inserindo_contato(cpf, nome, ativo, valor_total):
-	cliente = Customer(cpf, nome, ativo, valor_total)	
-	handler.insert(cliente)	
+def insert_customer(cpf, name, active, total_value):
+	c = Customer(cpf, name, active, total_value)	
+	handler.insert(c)	
 
-def busca_valor_maior_que_560_entre_1500_e_2700_com_media():
-	return handler.get_total_sum_and_customers_based_in(560.00, (1500, 2700))
+def do_search():
+	return handler.get_total_sum_and_customers_based_in(value=560.00, interval=(1500, 2700))
 
 
-nomes = ['Joao', 'Augusto', 'Maria', 'Pedro', 'Radames', 
+names = ['Joao', 'Augusto', 'Maria', 'Pedro', 'Radames', 
 		 'Severino', 'Arnold', 'Joana', 'Camila', 'Angelica',
 		 'Andre', 'Jeniffer', 'Paola', 'Otavio', 'Tarcisio']
 
-nomes_do_meio = ['Rodrigues', 'Souza', 'Ferreira', 'Lopes', 'Benevides',
+second_names = ['Rodrigues', 'Souza', 'Ferreira', 'Lopes', 'Benevides',
 				 'Schneider', 'Buarque', 'Smith', 'Sato', 'Abdalla',
 				 'Vieira', 'Knitell', 'Bach', 'Cobain', 'Pereira']
 
-nome_final = ['dos Santos', 'da Silva', 'von Neumann', 'de Alencar', 'de Paiva',
+last_names = ['dos Santos', 'da Silva', 'von Neumann', 'de Alencar', 'de Paiva',
 			  'da Villa', 'Nunes', 'Facchini', 'Bhaskara', 'Lennon',
 			  'Zuckerberg', 'Stradivarius', 'Klinsmann', 'McDonald', 'Kong']				
+
 
 for i in range(1, 3001):
 	x,y,z = randint(0,14), randint(0,14), randint(0,14)
 
-	nome = "Cliente {} {} {}".format(nomes[x], nomes_do_meio[y], nome_final[z])		
-	valor = (randint(100, 99999)) / 100.00
+	name = "{} {} {}".format(names[x], second_names[y], last_names[z])		
+	value = (randint(100, 99999)) / 100.00
 	rg = randint(11111111111, 99999999999)
-	ativo = randint(0,1)
+	active = randint(0,1)
 	
-	inserindo_contato(rg, nome, ativo, valor)
+	insert_customer(rg, name, active, value)
 
 
-r = busca_valor_maior_que_560_entre_1500_e_2700_com_media()
+dict_customers_and_media = do_search()
 
-print("Clientes...")
-for customer in r['customers']:
-	print(customer)
+print('______________________________________________________________________________________________________________')
+for customer in dict_customers_and_media['customers']:	
+	print('id: %4d | cpf: %11d | nome: %33s | ativo?: %3s | saldo total: %10.2f |' 
+		% (customer[0], customer[1], customer[2], 'sim' if customer[3] else 'nao', customer[4])) 
+	print('______________________________________________________________________________________________________________')
 
-print("Media ", r['media'])
+print('______________________________________________________________________________________________________________')
+print("Media geral de saldo dos clientes com ID entre 1500 e 2700: %10.2f" % dict_customers_and_media['media'][0])
 
 
 
